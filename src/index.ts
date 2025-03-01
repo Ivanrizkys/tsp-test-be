aff5f8716a82f8bc05cb8ce5d794ed6fc147f62d
ivan.rizky@gps.id <ivan.rizky@gps.id>
2025-03-01 20:13:53 +0700

    refactor: standardize root route response using common utility

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+import { sendResponseSuccess } from "#/common/utils/send-response.ts";
 import express from "express";
-
 const app = express();
 const port = process.env.PORT ?? "9000";
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  sendResponseSuccess(res, null, {
+    message: "Hello World",
+  });
 });
 
 app.listen(port, () => {
