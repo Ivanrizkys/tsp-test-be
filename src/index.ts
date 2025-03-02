ff143bec00f5244b389b70a38de0c40a49acb501
ivan.rizky@gps.id <ivan.rizky@gps.id>
2025-03-02 15:58:36 +0700

    feat: set up Express routes with authentication and health check endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import { sendResponseSuccess } from "#/common/utils/send-response.ts";
+import { authRouter } from "#/api/auth/auth-router.ts";
+import { healthCheckRouter } from "#/api/health-check/health-check-router.ts";
+import { errorMiddleware } from "#/common/middleware/error-middleware.ts";
 import express from "express";
+
 const app = express();
 const port = process.env.PORT ?? "9000";
 
-app.get("/", (req, res) => {
-  sendResponseSuccess(res, null, {
-    message: "Hello World",
-  });
-});
+app.use(express.json());
+
+app.use("/health-check", healthCheckRouter);
+app.use("/auth", authRouter);
+
+app.use(errorMiddleware);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
