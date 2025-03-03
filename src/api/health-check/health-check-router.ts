8176767373808b475d9653ae22077b0f8a2233af
ivan.rizky@gps.id <ivan.rizky@gps.id>
2025-03-03 09:55:15 +0700

    feat: enhance health check endpoint with database connection verification

--- src/api/health-check/health-check-router.ts
+++ src/api/health-check/health-check-router.ts
@@ -1,9 +1,12 @@
+import db from "#/common/config/database/connection.ts";
+import { usersTable } from "#/common/config/database/schema.ts";
 import { sendResponseSuccess } from "#/common/utils/send-response.ts";
 import { Request, Response, Router } from "express";
 
 export const healthCheckRouter = Router();
 
-healthCheckRouter.get("/", (req: Request, res: Response) => {
+healthCheckRouter.get("/", async (req: Request, res: Response) => {
+  await db.select().from(usersTable).limit(1);
   sendResponseSuccess(res, null, {
     message: "Server is running",
   });
